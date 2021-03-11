e657659064777a096af6dd9f89e81a82aea6c98b
WIIZZPL <WIIZZ_PL@outlook.com>
2021-03-11 11:10:16 +0100

    Create table if it doesn't exist

--- BookBasePrototype.py
+++ BookBasePrototype.py
@@ -7,29 +7,19 @@ cursor = db.cursor()
 
 #Creating the tables in the database
 
-try:
-    cursor.execute("""CREATE TABLE books (
-            ID integer,
-            Title text,
-            Author integer,
-            Subject integer,
-            Owned bool,
-            Read bool
-        )""")
-except sqlite3.OperationalError:
-    pass
-try:
-    cursor.execute("""CREATE TABLE authors (
-            ID integer,
-            Author text
-        )""")
-except sqlite3.OperationalError:
-    pass
-
-try:
-    cursor.execute("""CREATE TABLE subjects (
-            ID integer,
-            Subject text
-        )""")
-except sqlite3.OperationalError:
-    pass
+cursor.execute("""CREATE TABLE IF NOT EXISTS books (
+        ID integer PRIMARY KEY,
+        Title text NOT NULL,
+        Author integer NOT NULL,
+        Subject integer NOT NULL,
+        Owned bool DEFAULT false,
+        Read bool DEFAULT false
+    )""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS authors (
+        ID integer PRIMARY KEY,
+        Author text NOT NULL
+    )""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS subjects (
+        ID integer PRIMARY KEY,
+        Subject text NOT NULL
+    )""")
